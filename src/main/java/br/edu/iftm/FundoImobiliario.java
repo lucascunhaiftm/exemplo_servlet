85b74171ae62ea1f3b66e6909e7533f685b0e8aa
Lucas Cunha <lucas.cunha.iftm@gmail.com>
2025-08-07 15:11:33 -0300

    Criando classe de modelo do FundoImobiliario

---
+++ src/main/java/br/edu/iftm/FundoImobiliario.java
@@ -0,0 +1,26 @@
+package br.edu.iftm;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class FundoImobiliario {
+    private String nome;
+    private String setor;
+    private double preco;
+    private Date dataIpo;
+
+    public FundoImobiliario(String nome, String setor, double preco, Date dataIpo) {
+        this.nome = nome;
+        this.setor = setor;
+        this.preco = preco;
+        this.dataIpo = dataIpo;
+    }
+
+    public String getNome() { return nome; }
+    public String getSetor() { return setor; }
+    public double getPreco() { return preco; }
+    public String getDataFormatada() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        return sdf.format(dataIpo);
+    }
+}
